1d56ece094e6c3fe5edb6a151cf9b00d4ce986de
Danila Kutenin <kutdanila@yandex.ru>
2016-02-08 03:17:24 +0300

    Now it works, works properly

---
+++ build/CMakeFiles/tester.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tester.dir/main.cpp.o"
+  "CMakeFiles/tester.dir/huffman/Huffman.cpp.o"
+  "CMakeFiles/tester.dir/tester/tester.cpp.o"
+  "tester.pdb"
+  "tester"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
