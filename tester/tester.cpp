f9c694f965aeacaa2cc588f6e44f6fe84b3c211b
Danila Kutenin <kutdanila@yandex.ru>
2016-06-04 10:55:48 +0300

    sorry 1000000 bytes

--- tester/tester.cpp
+++ tester/tester.cpp
@@ -188,7 +188,7 @@ void Tester::test_encode_decode() {
   printf("decode ended in %f\n", (time_dec) / CLOCKS_PER_SEC);
   printf("Memory saved (MBs): %f\n", 1.0 * ((long long)mem2 - (long long)mem1) / 1024 / 1024);
   // max dict size is 1mb in average
-  printf("Memory saved (percent): %f%%\n", 100 - 100.0 * (mem1 + 1000) / mem2);
+  printf("Memory saved (percent): %f%%\n", 100 - 100.0 * (mem1 + 1000000) / mem2);
   printf("%zu errors were occured\n", error_count);
 }
 
@@ -259,7 +259,7 @@ void Tester::saved_memory() {
     ++encoded_it;
   }
   printf("Memory saved (MBs): %f\n", 1.0 * saved / 1024 / 1024);
-  printf("Memory saved (percent): %f%%\n", 100 - 100.0 * (mem1 + 1000) / mem2);
+  printf("Memory saved (percent): %f%%\n", 100 - 100.0 * (mem1 + 1000000) / mem2);
 }
 
 void Tester::reset() {
